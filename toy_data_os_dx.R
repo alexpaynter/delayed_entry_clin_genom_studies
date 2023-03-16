5a5d4829b0a9fa811197c200b695c6220dc10817
alexpaynter <alexcpaynter@gmail.com>
2023-03-16 10:11:42 -0700

    Added some diagnostics for lack of independence.

--- toy_data_os_dx.R
+++ toy_data_os_dx.R
@@ -7,6 +7,8 @@
 library(tidyverse)
 library(survival)
 library(survminer)
+# adding this on for quasi-independence testing:
+library(tranSurv)
 
 ## Load data
 toy_data = read.csv(here::here("toy_data.csv"))
@@ -18,15 +20,54 @@ toy_data = read.csv(here::here("toy_data.csv"))
 ## tt_cpt_report_mos = time (months) from date of cancer diagnosis to date of genomic sequencing
 
 ## Calculate median overall survival from diagnosis for total population
-survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data)
+survfit(Surv(time = tt_os_dx_mos, 
+             event = os_status_dx) ~ 1, 
+        data = toy_data)
 
 ## Subset data on those who underwent genomic sequencing
-toy_data_subset = toy_data %>% 
+toy_data_subset <- toy_data %>% 
   filter(cpt_status == 1)
 
 ## Calculate unadjusted median overall survival from diagnosis for subsetted population
-survfit(Surv(tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data_subset)
+survfit(
+  Surv(tt_os_dx_mos, 
+       os_status_dx) ~ 1, 
+  data = toy_data_subset)
 
 ## Calculate median overall survival from diagnosis for subsetted population after left truncation adjustment
-survfit(Surv(time = tt_cpt_report_mos, time2 = tt_os_dx_mos, os_status_dx) ~ 1, data = toy_data_subset)
+survfit(
+  Surv(time = tt_cpt_report_mos, 
+       time2 = tt_os_dx_mos, 
+       event = os_status_dx) ~ 1, 
+  data = toy_data_subset)
+
+
+
+
+
+# Quasi-independence testing of time to survival and time to genomic screening.
+# Martin and Betensky (2005)
+with(toy_data_subset,
+     tranSurv::cKendall(trun = tt_cpt_report_mos,
+                        obs = tt_os_dx_mos,
+                        delta = os_status_dx)
+)
+
+# Austin and Betensky (2014) inverse prob weighted version.
+with(toy_data_subset,
+     tranSurv::cKendall(trun = tt_cpt_report_mos,
+                        obs = tt_os_dx_mos,
+                        delta = os_status_dx,
+                        method = "IPW1")
+)
+
+# Austin and Betensky (2014) inverse prob weighted version #2
+with(toy_data_subset,
+     tranSurv::cKendall(trun = tt_cpt_report_mos,
+                        obs = tt_os_dx_mos,
+                        delta = os_status_dx,
+                        method = "IPW1")
+)
+
+
 
